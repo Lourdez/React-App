ab4e0861ff4c6143a0fdb6bd41a2c8a474f4668d
parker <54675124+Lourdez@users.noreply.github.com>
2024-09-18 17:01:57 +0530

    First Commit - react-app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+function App() {
+   return(
+    <div>
+   <section>
+        <nav class="navbar">
+            <div class="nav-left">
+                <ul class="nav-list">
+                    <li><a href="#home">AI cover letter generator</a></li>
+                    <li><a href="#about">resume generator</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                </ul>
+            </div>
+  
+        </nav>
+    </section>
+    </div>
+   )
+}
+
+export default App
